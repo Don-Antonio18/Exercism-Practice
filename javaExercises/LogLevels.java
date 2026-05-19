eb5819c68fa5d51d325c22454ecb044305747863
Antonio Kerr <antoniokerrrocks@gmail.com>
2026-05-19 17:09:23 -0500

    Finished LogLevel

---
+++ javaExercises/LogLevels.java
@@ -0,0 +1,21 @@
+public class LogLevels {
+    // Logline format: "[ERROR]: Invalid operation"
+
+    // Returns a log line's message
+    public static String message(String logLine) {
+        return logLine.split("]: ")[1].trim();
+    }
+
+    // Returns a log line's level in lowercase
+    public static String logLevel(String logLine) {
+        return logLine.split("]: ")[0].substring(1).toLowerCase();
+    }
+
+    // Reformats log line such that message is first
+    public static String reformat(String logLine) {
+        String msg = message(logLine);
+        String loglevel = logLevel(logLine);
+        String reformString = String.format("%s (%s)", msg, loglevel);
+        return reformString;
+    }
+}
